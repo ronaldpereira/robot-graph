5995ff2c09a5b28b5da636517cea9aad3aecbe1f
ronaldpereira <ronald.drp11@gmail.com>
2016-05-31 17:22:10 -0300

    Inclusão dos testes toy

---
+++ testes/run_toys.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+./tp2.exe toyA.txt 0 0 1 2 0 0 # resp: 1028
+./tp2.exe toyA.txt 0 0 1 2 1 3 # resp: -1
+./tp2.exe toyA.txt 0 1 4 3 2 1 # resp: 1120
+./tp2.exe toyB.txt 0 1 0 9 0 0 # resp: 2992
+./tp2.exe toyB.txt 7 7 2 6 1 2 # resp: -1
+./tp2.exe toyB.txt 7 7 0 0 1 1 # resp: 4254
